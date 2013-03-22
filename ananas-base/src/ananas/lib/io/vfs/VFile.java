2b17e443df9bf1585776ff9b349cd6c9f6efff8f
xukun <xukun0217@gmail.com>
2013-03-23 05:21:10 +0800

    add: ananas.lib.io.vfs.*

--- ananas-base/src/ananas/lib/io/vfs/VFile.java
+++ ananas-base/src/ananas/lib/io/vfs/VFile.java
@@ -1,6 +1,25 @@
 package ananas.lib.io.vfs;
 
+import java.io.IOException;
+import java.net.URI;
+
 public interface VFile {
 
 	VFileSystem getVFS();
+
+	boolean exists();
+
+	boolean mkdirs();
+
+	boolean isDirectory();
+
+	boolean createNewFile() throws IOException;
+
+	VFile getParentFile();
+
+	boolean isFile();
+
+	URI getURI();
+
+	String getName();
 }
--- ananas-base/src/ananas/lib/io/vfs/VFileSystem.java
+++ ananas-base/src/ananas/lib/io/vfs/VFileSystem.java
@@ -1,20 +1,21 @@
 package ananas.lib.io.vfs;
 
+import java.net.URI;
+
 public interface VFileSystem {
 
 	VFileSystemFactory getFactory();
 
 	VFileSystemConfiguration getConfiguration();
 
-	class Factory {
+	// new file
 
-		public static VFileSystemFactory getFactory() {
-			return VFileSystemBootstrap.getFactory(null);
-		}
+	VFile newFile(VFile dir, String string);
 
-		public static VFileSystemFactory getFactory(String classname) {
-			return VFileSystemBootstrap.getFactory(classname);
-		}
-	}
+	VFile newFile(String dir, String string);
+
+	VFile newFile(String path);
+
+	VFile newFile(URI uri);
 
 }
